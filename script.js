f1c9004458608cc12c43ca16ffbebb44046c7970
emmanueljose <emjose@gmail.com>
2022-09-23 13:46:35 -0400

    update structure and styling

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const password = document.getElementById('password')
-const background = document.getElementById('background')
+"use strict";
 
-password.addEventListener('input', (e) => {
-    const input = e.target.value
-    const length = input.length
-    const blurValue = 20 - length * 2
-    background.style.filter = `blur(${blurValue}px)`
-})
+const password = document.getElementById("password");
+const background = document.getElementById("background");
+
+password.addEventListener("input", (e) => {
+	const input = e.target.value;
+	const length = input.length;
+	const blurValue = 20 - length * 2;
+	background.style.filter = `blur(${blurValue}px)`;
+});
